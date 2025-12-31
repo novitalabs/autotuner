3ea77df43b33ebb2403e01ce26c4efe4c7a61e4e
K.L. Λ <k.l.lambda@gmail.com>
2025-12-31 08:13:11 +0000

    Widen running task panel.

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -947,11 +947,11 @@ export default function Dashboard() {
 			</div>
 
 			{/* Second row: Running Tasks (left) + Timeline (right) */}
-			<div className="grid grid-cols-1 lg:grid-cols-4 gap-6 mb-6">
+			<div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mb-6">
 				<div className="lg:col-span-1">
 					{renderRunningTasksCard()}
 				</div>
-				<div className="lg:col-span-3">
+				<div className="lg:col-span-2">
 					{renderTimelineChart()}
 				</div>
 			</div>
